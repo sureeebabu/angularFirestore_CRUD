600aced79ecf9f029468f93ebc6ee914522e4c3c
daijichennai <daijiworldtech@gmail.com>
2019-03-24 14:29:43 +0530

    Query is added in shared/service.ts

--- src/app/shared/employee.service.ts
+++ src/app/shared/employee.service.ts
@@ -9,6 +9,11 @@ export class EmployeeService {
   formData: Employee;
   constructor(private firestore: AngularFirestore) { }
   getEmployees() {
-    return this.firestore.collection('employees').snapshotChanges();
+    return this.firestore.collection('employees', ref =>
+    ref .orderBy('empCode')
+        .limit(10)
+        // .startAt('ask')Auto Complete -> https://angularfirebase.com/lessons/typeahead-autocomplete-with-firestore/
+        // .endAt('ask\uf8ff')
+    ).snapshotChanges();
   }
 }
